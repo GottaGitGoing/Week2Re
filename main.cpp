741bc34c24d28248043b130eeb05bcfe6756e27a
Arian <aryannamavar@gmail.com>
2020-04-07 17:44:00 -0700

    Added Basic Getlines

---
+++ main.cpp
@@ -0,0 +1,22 @@
+#include <iostream>
+#include <string>
+
+
+int main()
+{
+
+    // int i = 0;
+    int limit;
+    std::string company;
+    std::string description;
+    std::cout << "enter limit" <<std::endl;
+    std::cin>>limit;
+    std::cin.ignore(1);
+    std::cout << "enter companyt" <<std::endl;
+    getline(std::cin,company);
+    std::cout << "enter descitpoin" <<std::endl;
+    getline(std::cin,description);
+
+    
+}
+
