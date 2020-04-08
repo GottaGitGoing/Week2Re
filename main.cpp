30690192d6908d8cb010d2c2c9f213cc44ca121b
Arian <aryannamavar@gmail.com>
2020-04-07 20:21:51 -0700

    Added while loop

--- main.cpp
+++ main.cpp
@@ -5,18 +5,27 @@
 int main()
 {
 
-    // int i = 0;
+    int i = 0;
     int limit;
     std::string company;
     std::string description;
-    std::cout << "enter limit" <<std::endl;
+    // std::cout << "enter limit" <<std::endl;
     std::cin>>limit;
     std::cin.ignore(1);
-    std::cout << "enter companyt" <<std::endl;
+    // std::cout << "enter companyt" <<std::endl;
     getline(std::cin,company);
-    std::cout << "enter descitpoin" <<std::endl;
+    // std::cout << "enter descitpoin" <<std::endl;
     getline(std::cin,description);
-
+    int amount;
+    double price;
+    std::string st_name;
+    std::cout << description << " (" << company << ")" << std::endl;
+    while (i<limit)
+    {
+        std::cin >> amount >> price >> st_name;
+        i++;
+        std::cout << st_name << ": " << amount*price << std::endl;
+    }
     
 }
 
